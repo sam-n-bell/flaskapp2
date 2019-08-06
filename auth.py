f0fb68b581efef4e7b8e55372cd6018e353775af
sambell <bell.sam@Outlook.com>
2019-08-05 19:57:54 -0500

    testing

--- auth.py
+++ auth.py
@@ -3,8 +3,8 @@ import jwt
 from jwt.contrib.algorithms.pycrypto import RSAAlgorithm
 from jwt.contrib.algorithms.py_ecdsa import ECAlgorithm
 
-jwt.unregister_algorithm('RS256')
-jwt.unregister_algorithm('ES256')
+# jwt.unregister_algorithm('RS256')
+# jwt.unregister_algorithm('ES256')
 
 jwt.register_algorithm('RS256', RSAAlgorithm(RSAAlgorithm.SHA256))
 jwt.register_algorithm('ES256', ECAlgorithm(ECAlgorithm.SHA256))
